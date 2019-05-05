cb36f5886bc6944414213658b25fc1f4319328b4
tmr08c <tmr08c@gmail.com>
2019-05-05 18:46:11 -0400

    Updates favicon.
    favicon created using [this site](https://www.favicon.cc/).
    
    Also removes old favicon and another unnecessary image.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,24 +1,24 @@
 module.exports = {
   siteMetadata: {
-    title: 'TroyProg',
+    title: "TroyProg",
     description:
-      'This is the personal site for Troy Rosenberg - engineering manager and develop. This serves as a place to store blog posts that are generally about technology. Topics may include - Ruby, Ruby on Rails, Elixir, Phoenix, JavaScript, TypeScript, Node, project management, engineering mangement, and more!',
-    author: 'Troy Rosenberg (@tmr08c)',
+      "This is the personal site for Troy Rosenberg - engineering manager and develop. This serves as a place to store blog posts that are generally about technology. Topics may include - Ruby, Ruby on Rails, Elixir, Phoenix, JavaScript, TypeScript, Node, project management, engineering mangement, and more!",
+    author: "Troy Rosenberg (@tmr08c)"
   },
   plugins: [
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/content/blog`,
-        name: 'blog',
-      },
+        name: "blog"
+      }
     },
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
         path: `${__dirname}/content/assets`,
-        name: 'assets',
-      },
+        name: "assets"
+      }
     },
     {
       resolve: `gatsby-transformer-remark`,
@@ -27,66 +27,62 @@ module.exports = {
           {
             resolve: `gatsby-remark-images`,
             options: {
-              maxWidth: 500,
-            },
+              maxWidth: 500
+            }
           },
           {
-            resolve: 'gatsby-remark-external-links',
+            resolve: "gatsby-remark-external-links",
             options: {
-              target: '_blank',
-              rel: 'nofollow noopener noreferrer',
-            },
+              target: "_blank",
+              rel: "nofollow noopener noreferrer"
+            }
           },
           `gatsby-remark-responsive-iframe`,
           {
-            resolve: `gatsby-remark-prismjs`,
+            resolve: `gatsby-remark-prismjs`
           },
-          `gatsby-remark-copy-linked-files`,
-        ],
-      },
+          `gatsby-remark-copy-linked-files`
+        ]
+      }
     },
-    'gatsby-plugin-typescript',
-    'gatsby-plugin-react-helmet',
+    "gatsby-plugin-typescript",
+    "gatsby-plugin-react-helmet",
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+        path: `${__dirname}/src/images`
+      }
     },
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
-        start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
-      },
+        name: "troy-prog-blog",
+        short_name: "troyprog",
+        start_url: "/"
+      }
     },
-    'gatsby-plugin-offline',
+    "gatsby-plugin-offline",
     `gatsby-plugin-postcss`,
     {
       resolve: `gatsby-plugin-prefetch-google-fonts`,
       options: {
         fonts: [
           {
-            family: `Chewy`,
-          },
-        ],
-      },
+            family: `Chewy`
+          }
+        ]
+      }
     },
     `gatsby-plugin-catch-links`,
     `gatsby-transformer-json`,
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
-        path: `${__dirname}/content/data`,
-      },
-    },
-  ],
-}
+        path: `${__dirname}/content/data`
+      }
+    }
+  ]
+};
