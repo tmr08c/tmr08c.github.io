9959e003a180c200dbf3e59f3c323f805d96ea01
Troy Rosenberg <tmr08c@gmail.com>
2021-08-03 06:57:49 -0400

    Run formatter

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,22 +4,22 @@ module.exports = {
     description:
       "This is the personal site for Troy Rosenberg - engineering manager and develop. This serves as a place to store blog posts that are generally about technology. Topics may include - Ruby, Ruby on Rails, Elixir, Phoenix, JavaScript, TypeScript, Node, project management, engineering mangement, and more!",
     author: "Troy Rosenberg (@tmr08c)",
-    repsitory: "https://github.com/tmr08c/tmr08c.github.io"
+    repsitory: "https://github.com/tmr08c/tmr08c.github.io",
   },
   plugins: [
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/content/blog`,
-        name: "blog"
-      }
+        name: "blog",
+      },
     },
     {
       resolve: "gatsby-source-filesystem",
       options: {
         path: `${__dirname}/content/assets`,
-        name: "assets"
-      }
+        name: "assets",
+      },
     },
     {
       resolve: `gatsby-transformer-remark`,
@@ -28,28 +28,28 @@ module.exports = {
           {
             resolve: `gatsby-remark-images`,
             options: {
-              maxWidth: 500
-            }
+              maxWidth: 500,
+            },
           },
           {
             resolve: "gatsby-remark-external-links",
             options: {
               target: "_blank",
-              rel: "nofollow noopener noreferrer"
-            }
+              rel: "nofollow noopener noreferrer",
+            },
           },
           `gatsby-remark-responsive-iframe`,
           {
             resolve: `gatsby-remark-vscode`,
             options: {
               theme: `Tomorrow`,
-              extensions: [`elixir-ls`, `viml`, `vscode-themes/tomorrow`]
-            }
+              extensions: [`elixir-ls`, `viml`, `vscode-themes/tomorrow`],
+            },
           },
           `gatsby-remark-copy-linked-files`,
-          `gatsby-remark-autolink-headers`
-        ]
-      }
+          `gatsby-remark-autolink-headers`,
+        ],
+      },
     },
     "gatsby-plugin-typescript",
     "gatsby-plugin-react-helmet",
@@ -57,8 +57,8 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`
-      }
+        path: `${__dirname}/src/images`,
+      },
     },
     "gatsby-transformer-sharp",
     "gatsby-plugin-sharp",
@@ -68,8 +68,8 @@ module.exports = {
         name: "troy-prog-blog",
         short_name: "troyprog",
         start_url: "/",
-        icon: "src/images/RhiDesign Troy Avatar.png"
-      }
+        icon: "src/images/RhiDesign Troy Avatar.png",
+      },
     },
     "gatsby-plugin-offline",
     `gatsby-plugin-postcss`,
@@ -78,18 +78,18 @@ module.exports = {
       options: {
         fonts: [
           {
-            family: `Chewy`
-          }
-        ]
-      }
+            family: `Chewy`,
+          },
+        ],
+      },
     },
     `gatsby-plugin-catch-links`,
     `gatsby-transformer-json`,
     {
       resolve: "gatsby-source-filesystem",
       options: {
-        path: `${__dirname}/content/data`
-      }
-    }
-  ]
+        path: `${__dirname}/content/data`,
+      },
+    },
+  ],
 };
