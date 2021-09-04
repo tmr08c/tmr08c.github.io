bc6eafbc9517a691af7b1d1258e468fde06a94d2
Troy Rosenberg <troy.rosenberg@cpowerenergymanagement.com>
2021-09-04 16:06:09 -0400

    Update header to always fetch SiteName
    Originally, I didn't have the SiteName in the headers on the home page.
    Due to the size of the site name, this would cause the other elements to
    shift when switching between the home page and other pages on the site.
    
    Instead, we will now always include the site name to keep the header
    standard.
    
    Since we are now keeping the site name, we could move the `StaticQuery`
    to fetch it from the Gatsby site metadata out of the `Layout` and intot
    the header iteself.

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,30 +1,16 @@
-import { graphql, StaticQuery } from "gatsby";
 import * as React from "react";
 
 import Footer from "./Footer";
 import Header from "./Header";
 
 const Layout: React.SFC = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `}
-    render={(data) => (
-      <>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <main className="min-h-screen mx-auto px-4 py-8 max-w-2xl sm:px-6 sm:py-12 lg:max-w-3xl lg:py-16 2xl:max-w-4xl xl:px-0">
-          {children}
-        </main>
-        <Footer />
-      </>
-    )}
-  />
+  <>
+    <Header />
+    <main className="min-h-screen mx-auto px-4 py-8 max-w-2xl sm:px-6 sm:py-12 lg:max-w-3xl lg:py-16 2xl:max-w-4xl xl:px-0">
+      {children}
+    </main>
+    <Footer />
+  </>
 );
 
 export default Layout;
