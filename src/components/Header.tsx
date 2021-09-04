98c09493f7fbca1692983f1b15ec2eaa79c64fd9
Troy Rosenberg <troy.rosenberg@cpowerenergymanagement.com>
2021-09-04 16:06:09 -0400

    Update header to always fetch SiteName
    Originally, I didn't have the SiteName in the headers on the home page.
    Due to the size of the site name, this would cause the other elements to
    shift when switching between the home page and other pages on the site.
    
    Instead, we will now always include the site name to keep the header
    standard.
    
    Since we are now keeping the site name, we could move the `StaticQuery`
    to fetch it from the Gatsby site metadata out of the `Layout` and intot
    the header iteself.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,31 +1,41 @@
-import { Link } from "gatsby";
+import { Link, StaticQuery, graphql } from "gatsby";
 import * as React from "react";
 
-interface HeaderProps {
-  siteTitle?: string;
+function Header(): JSX.Element {
+  return (
+    <StaticQuery
+      query={graphql`
+      query SiteTitleQuery {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `}
+      render={data => (
+        <nav className="flex items-center justify-between flex-wrap bg-green-800 px-6 py-12 mb-5 text-white">
+          <div className="flex flex-no-shrink">
+            <Link to="/" className="font-semibold text-4xl tracking-tighter">
+              {data.site.siteMetadata.title}
+            </Link>
+          </div>
+          <div className="justify-end flex mr-4">
+            <div className="text-xl">
+              <Link to="/talks" className="mr-6">
+                Talks
+              </Link>
+
+              <Link to="/blog">Blog</Link>
+            </div>
+          </div>
+        </nav>
+      )} />
+  );
 }
 
-const Header: React.SFC<HeaderProps> = ({ siteTitle }) => (
-  <nav className="flex items-center justify-between flex-wrap bg-green-800 px-6 py-12 mb-5 text-white">
-    <div className="flex flex-no-shrink">
-      <Link to="/" className="font-semibold text-4xl tracking-tighter">
-        {siteTitle}
-      </Link>
-    </div>
-    <div className="justify-end flex mr-4">
-      <div className="text-xl">
-        <Link to="/talks" className="mr-6">
-          Talks
-        </Link>
-
-        <Link to="/blog">Blog</Link>
-      </div>
-    </div>
-  </nav>
-);
-
 Header.defaultProps = {
-  siteTitle: "",
+  siteTitle: "STuff",
 };
 
 export default Header;
