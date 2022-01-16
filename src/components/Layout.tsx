7a6437de9a216b1c33e68fca7c6a4b1cd460b64b
Troy Rosenberg <troy.rosenberg@cpowerenergymanagement.com>
2022-01-15 19:06:47 -0500

    (site) Change main font used throughout site
    Add the [ET Book](https://edwardtufte.github.io/et-book/) font to the
    project, add it to the list of Tailwind serif fonts, and update our
    `main` element to use `font-serif`.

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,7 +6,7 @@ import Header from "./Header";
 const Layout: React.SFC = ({ children }) => (
   <div className="dark:bg-gray-800 dark:text-white">
     <Header />
-    <main className="min-h-screen mx-auto px-4 py-8 max-w-2xl sm:px-6 sm:py-12 lg:max-w-3xl lg:py-16 2xl:max-w-4xl xl:px-0">
+    <main className="min-h-screen mx-auto px-4 py-8 max-w-2xl sm:px-6 sm:py-12 lg:max-w-3xl lg:py-16 2xl:max-w-4xl xl:px-0 font-serif">
       {children}
     </main>
     <Footer />
