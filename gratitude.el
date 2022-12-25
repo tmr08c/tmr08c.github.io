6ce31cdef1cfa3aa5acd7387c7ad9127b04333da
Troy Rosenberg <tmr08c@gmail.com>
2022-12-25 13:48:17 -0500

    Attepmting to start at the headline

--- gratitude.el
+++ gratitude.el
@@ -27,21 +27,105 @@
 (require 'org)
 (require 'org-element)
 
-(progn
-  (find-file-other-window "~/Dropbox/Notes/org/roam/daily/2022-11-18.org")
-  (let ((lists
-         (org-element-map (org-element-parse-buffer) 'plain-list
-           ;; TODO also fiter on parent headling
-           ;; alternatively, I may be able to get the headline first and then get the children content
-           (lambda (list) (and
-                      (string-equal (org-element-property :type list) 'ordered)
-                      list)))))
-    (print lists)
-        )
-    ;; (other-window 2)
-    ;; (goto-char (point-max))
-    ;; (insert (format "%s" org-ast))
-    )
+(defun gratitude () "Get gratitude section of daily files."
+       (find-file-other-window "~/Dropbox/Notes/org/roam/daily/2022-11-18.org")
+       (let ((lists
+              (org-element-map (org-element-parse-buffer) 'plain-list
+                ;; TODO also fiter on parent headling
+                ;; alternatively, I may be able to get the headline first and then get the children content
+                (lambda (list)
+                  ;; (print list)
+                  ;; (print (length list))
+                  (cl-destructuring-bind (type plist . children) list
+                    ;; (print "TYPE:")
+                    ;; (print type)
+                    ;; (print "==============================")
+                    ;; (print "PLIST:")
+                    ;; (print plist)
+                    ;; (print "==============================")
+                    ;; (print "CHILDREN:")
+                    ;; (print children)
+                    ;; (print "==============================")
+                    (and
+                     (string-equal (org-element-property :type list) 'ordered)
+                     (org-element-map (org-element-property :parent list) 'headline
+                       (lambda (headline)
+                         (print headline)
+                         (and
+                         (print (org-element-property :raw-value headline))
+                         (string-equal (org-element-property :raw-value headline) "Grateful or Excited About")
+                         list)))
+                     ;; (print (org-element-property :parent list))
+                     list))))))
+         (print "hum..")
+         (print (format "list has %s elements" (length lists)))
+         )
+       ;; (other-window 2)
+       ;; (goto-char (point-max))
+       ;; (insert (format "%s" org-ast))
+       )
 
-  (provide 'gratitude)
+(defun gratitude-from-headline () "Get gratitude section of daily files."
+       (find-file-other-window "~/Dropbox/Notes/org/roam/daily/2022-11-18.org")
+       (let ((lists
+              (org-element-map (org-element-parse-buffer) 'headline
+                ;; TODO also fiter on parent headling
+                ;; alternatively, I may be able to get the headline first and then get the children content
+                (lambda (headline)
+                  ;; (print list)
+                  ;; (print (length list))
+                  ;; (cl-destructuring-bind (type plist . children) headline
+                    ;; (print "TYPE:")
+                    ;; (print type)
+                    ;; (print "==============================")
+                    ;; (print "PLIST:")
+                    ;; (print plist)
+                    ;; (print "==============================")
+                    ;; (print "CHILDREN:")
+                    ;; (print children)
+                    ;; (print "==============================")
+                    (and
+                     (string-equal (org-element-property :raw-value headline) "Grateful or Excited About")
+                     (org-element-map headline 'plain-list
+                       (lambda (list)
+                         (and
+                          (string-equal (org-element-property :type list) 'ordered)
+                          list
+                          )))
+                     ;; (print (org-element-property :parent list))
+                     )))))
+         ;; )
+         (print (org-element-map lists 'item
+           (lambda (item) (org-element-map (org-element-contents item) 'paragraph
+                       (lambda (p) (format "%s" (org-element-normalize-contents p)))))))
+         (print "hmm..")
+         ;; (print lists)
+         (print (format "list has %s elements" (length lists)))
+         )
+       ;; (other-window 2)
+       ;; (goto-char (point-max))
+       ;; (insert (format "%s" org-ast))
+       )
+
+(gratitude-from-headline)
+;; (provide 'gratitude)
 ;;; gratitude.el ends here
+
+(gratitude)
+
+;; (defun tr/test ()
+;;   "testing function writing"
+;;   (with-output-to-temp-buffer "*debug org fun*"
+;;     (org-element-map (org-element-parse-buffer) 'headline
+;;       (lambda (headline)
+;;         (if (string= (org-element-property :raw-value headline) "Grateful or Excited About")
+;;             (org-element-map (org-element-contents headline) 'plain-list
+;;               (lambda (list)
+;;                 (if (eq (org-element-property :parent list) headline)
+;;                      (print (org-element-contents list))
+;;                      (org-element-map (org-element-contents list) 'item
+;;                        (lambda (item)
+;;                          (print (org-element-contents item)))))))
+;;           )))))
+
+;; (tr/test)
